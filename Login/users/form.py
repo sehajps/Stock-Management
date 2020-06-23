77fafea856742c0d9aa3317e69752563af236f31
sehajps <sehajpalsingh121@gmail.com>
2020-06-23 18:12:35 +0530

    spaces not allowrd

--- Login/users/form.py
+++ Login/users/form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,IntegerField,SelectField,DateField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired, Email, NumberRange,ValidationError,EqualTo
+from wtforms.validators import DataRequired, Email, NumberRange,ValidationError,EqualTo,Regexp
 from Login.models import user
 olimit=100
 class LoginForm(FlaskForm):
@@ -19,10 +19,10 @@ class inputForm(FlaskForm):
     place=SelectField('Place',validators=[DataRequired()],choices=[('office','Office'),('godown','Godown')])
     cell_no=IntegerField('Cell Number',validators=[DataRequired(),NumberRange(min=0,max=olimit,message='Please enter number in limit')])
     n_b=SelectField('Product',validators=[DataRequired()],choices=[('nut','Nut'),('bolt','Bolt'),('washer','Washer')])
-    size=StringField('Size',validators=[DataRequired()])
+    size=StringField('Size',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
     quantity=IntegerField('Quantity (in kg)',validators=[DataRequired(),NumberRange(min=0,message='Please enter number in limit')])
-    description=StringField('Description',validators=[DataRequired()])
-    sender=StringField('Sender',validators=[DataRequired()])
+    description=StringField('Description',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
+    sender=StringField('Sender',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
     submit=SubmitField('Submit')
 class searchCellForm(FlaskForm):
     place=SelectField('Place',validators=[DataRequired()],choices=[('office','Office'),('godown','Godown')])
@@ -30,12 +30,12 @@ class searchCellForm(FlaskForm):
     submit=SubmitField('Search')
 class updateForm(FlaskForm):
     quantity=IntegerField('Quantity',validators=[DataRequired(),NumberRange(min=0,message='Please enter number in limit')])
-    recipient=StringField('Recipient',validators=[DataRequired()])
+    recipient=StringField('Recipient',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
     submit=SubmitField('Update')
 class searchsizeForm(FlaskForm):
-    size=StringField('Size')
-    description=StringField('Description')
-    sender=StringField('Sender')
+    size=StringField('Size',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed")])
+    description=StringField('Description',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed")])
+    sender=StringField('Sender',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed")])
     submit=SubmitField('Search')
 
 class sortedlistForm(FlaskForm):
@@ -74,8 +74,8 @@ class editLogForm(FlaskForm):
     place=SelectField('Place',validators=[DataRequired()],choices=[('office','Office'),('godown','Godown')])
     cell_no=IntegerField('Cell Number',validators=[DataRequired(),NumberRange(min=0,max=olimit,message='Please enter number in limit')])
     n_b=SelectField('Product',validators=[DataRequired()],choices=[('nut','Nut'),('bolt','Bolt'),('washer','Washer')])
-    size=StringField('Size',validators=[DataRequired()])
+    size=StringField('Size',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
     quantity=IntegerField('Quantity',validators=[DataRequired(),NumberRange(min=0,message='Please enter number in limit')])
-    description=StringField('Description',validators=[DataRequired()])
-    recipient=StringField('Recipient',validators=[DataRequired()])
+    description=StringField('Description',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
+    recipient=StringField('Recipient',validators=[Regexp(r'^[\w.@+-]+$',message="Spaces are not allowed"),DataRequired()])
     submit=SubmitField('Submit')
